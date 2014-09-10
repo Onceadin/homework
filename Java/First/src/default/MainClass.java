5b7ecb786eecb5ca6c67ab3be9f20c5a7a0b4aed
Sokol Andrey <sokolandrey1993@mail.ru>
2014-09-11 01:09:29 +0400

    first commit

---
+++ Java/First/src/default/MainClass.java
@@ -0,0 +1,12 @@
+public class MainClass{
+	public static void main(String[] args){
+		String sep = args[0];
+		String[] text = args[1].split(sep);
+		int size = text.length;
+		if(size > 0){
+			for(int i = size - 1; i >= 0; i--){
+				System.out.println(text[i]);
+			}
+		}
+	}
+}
